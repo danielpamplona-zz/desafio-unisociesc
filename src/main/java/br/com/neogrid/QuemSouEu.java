7c17524c6b756beea0bffe107804acf88952dee4
Daniel Pamplona <daniel.soares@neogrid.com>
2018-10-09 14:30:59 -0300

    Desafio - commit inicial

---
+++ src/main/java/br/com/neogrid/QuemSouEu.java
@@ -0,0 +1,27 @@
+package br.com.neogrid;
+
+import br.com.neogrid.desafio.app.QuemSouEuInterface;
+
+public class QuemSouEu implements QuemSouEuInterface {
+
+	@Override
+	public String getNome() {
+		return "";
+	}
+
+	@Override
+	public String getEmail() {
+		return "";
+	}
+
+	@Override
+	public String getCelular() {
+		return "";
+	}
+
+	@Override
+	public String getCurso() {
+		return "";
+	}
+
+}
